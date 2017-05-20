1f35e162d8f6be9b0f227b187585584c124adc5b
Thomas Wiemann <twiemann@uos.de>
2017-05-20 13:50:40 +0200

    Moved io util fuctions from kaboom to IOUtils.

--- src/liblvr/io/IOUtils.cpp
+++ src/liblvr/io/IOUtils.cpp
@@ -99,4 +99,222 @@ Eigen::Matrix4d getTransformationFromFrames(boost::filesystem::path& frames)
     return buildTransformation(alignxf);
 }
 
+
+size_t countPointsInFile(boost::filesystem::path& inFile)
+{
+    std::ifstream in(inFile.c_str());
+    std::cout << timestamp << "Counting points in " << inFile.filename().string() << "..." << std::endl;
+
+    // Count lines in file
+    size_t n_points = 0;
+    char line[2048];
+    while(in.good())
+    {
+        in.getline(line, 1024);
+        n_points++;
+    }
+    in.close();
+
+    std::cout << timestamp << "File " << inFile.filename().string() << " contains " << n_points << " points." << std::endl;
+
+    return n_points;
+}
+
+void writeFrames(Eigen::Matrix4d transform, const boost::filesystem::path& framesOut)
+{
+    std::ofstream out(framesOut.c_str());
+
+    // write the rotation matrix
+    out << transform.col(0)(0) << " " << transform.col(0)(1) << " " << transform.col(0)(2) << " " << 0 << " "
+        << transform.col(1)(0) << " " << transform.col(1)(1) << " " << transform.col(1)(2) << " " << 0 << " "
+        << transform.col(2)(0) << " " << transform.col(2)(1) << " " << transform.col(2)(2) << " " << 0 << " ";
+
+    // write the translation vector
+    out << transform.col(3)(0) << " "
+        << transform.col(3)(1) << " "
+        << transform.col(3)(2) << " "
+        << transform.col(3)(3);
+
+    out.close();
+}
+
+size_t writeModel( ModelPtr model,const  boost::filesystem::path& outfile)
+{
+    size_t n_ip;
+    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
+
+    ModelFactory::saveModel(model, outfile.string());
+
+    return n_ip;
+}
+
+size_t writePointsToASCII(ModelPtr model, std::ofstream& out, bool nocolor)
+{
+    size_t n_ip, n_colors;
+
+    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
+
+    ucharArr colors = model->m_pointCloud->getPointColorArray(n_colors);
+    for(int a = 0; a < n_ip; a++)
+    {
+        out << arr[a * 3] << " " << arr[a * 3 + 1] << " " << arr[a * 3 + 2];
+
+        if(n_colors && !(nocolor))
+        {
+            out << " " << (int)colors[a * 3] << " " << (int)colors[a * 3 + 1] << " " << (int)colors[a * 3 + 2];
+        }
+        out << std::endl;
+
+    }
+
+    return n_ip;
+}
+
+size_t getReductionFactor(ModelPtr model, size_t reduction)
+{
+    size_t n_points;
+    floatArr arr = model->m_pointCloud->getPointArray(n_points);
+
+
+    std::cout << timestamp << "Point cloud contains " << n_points << " points." << std::endl;
+
+/*
+     * If reduction is less than the number of points it will segfault
+     * because the modulo operation is not defined for n mod 0
+     * and we have to keep all points anyways.
+     * Same if no targetSize was given.
+     */
+    if(reduction != 0)
+    {
+        if(reduction < n_points)
+        {
+            return (int)n_points / reduction;
+        }
+    }
+
+    /* No reduction write all points */
+    return 1;
+}
+
+size_t getReductionFactorASCII(boost::filesystem::path& inFile, size_t targetSize)
+{
+    /*
+     * If reduction is less than the number of points it will segfault
+     * because the modulo operation is not defined for n mod 0
+     * and we have to keep all points anyways.
+     * Same if no targetSize was given.
+     */
+    if(targetSize != 0)
+    {
+        // Count lines in file
+        size_t n_points = countPointsInFile(inFile);
+
+        if(targetSize < n_points)
+        {
+            return (int)n_points / targetSize;
+        }
+    }
+
+    /* No reduction write all points */
+    return 1;
+
+}
+
+void transformAndReducePointCloud(ModelPtr model, int modulo, int sx, int sy, int sz, int xPos, int yPos, int zPos)
+{
+    size_t n_ip, n_colors;
+    size_t cntr = 0;
+
+    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
+    ucharArr colors = model->m_pointCloud->getPointColorArray(n_colors);
+
+    // Plus one because it might differ because of the 0-index
+    // better waste memory for one float than having not enough space.
+    // TO-DO think about exact calculation.
+    size_t targetSize = (3 * ((n_ip)/modulo)) + modulo;
+    floatArr points(new float[targetSize ]);
+    ucharArr newColorsArr;
+
+    if(n_colors)
+    {
+        newColorsArr = ucharArr(new unsigned char[targetSize]);
+    }
+
+    for(int i = 0; i < n_ip; i++)
+    {
+        if(i % modulo == 0)
+        {
+            if(sx != 1)
+            {
+                arr[i * 3] 		*= sx;
+            }
+
+            if(sy != 1)
+            {
+                arr[i * 3 + 1] 	*= sy;
+            }
+
+            if(sz != 1)
+            {
+                arr[i * 3 + 2] 	*= sz;
+            }
+
+            if((cntr * 3) < targetSize)
+            {
+                points[cntr * 3]     = arr[i * 3 + xPos];
+                points[cntr * 3 + 1] = arr[i * 3 + yPos];
+                points[cntr * 3 + 2] = arr[i * 3 + zPos];
+            }
+            else
+            {
+                std::cout << "The following is for debugging purpose" << std::endl;
+                std::cout << "Cntr: " << (cntr * 3) << " targetSize: " << targetSize << std::endl;
+                std::cout << "nip : " << n_ip << " modulo " << modulo << std::endl;
+                break;
+            }
+
+            if(n_colors)
+            {
+                newColorsArr[cntr * 3]     = colors[i * 3];
+                newColorsArr[cntr * 3 + 1] = colors[i * 3 + 1];
+                newColorsArr[cntr * 3 + 2] = colors[i * 3 + 2];
+            }
+
+            cntr++;
+        }
+    }
+
+    // Pass counter because it is the actual number of points used after reduction
+    // it might be 1 less than the size
+    model->m_pointCloud->setPointArray(points, cntr);
+
+    if(n_colors)
+    {
+        model->m_pointCloud->setPointColorArray(newColorsArr, cntr);
+    }
+}
+
+
+void transformPointCloud(ModelPtr model, Eigen::Matrix4d transformation)
+{
+    std::cout << timestamp << "Transforming model." << std::endl;
+    size_t numPoints;
+
+    floatArr arr = model->m_pointCloud->getPointArray(numPoints);
+
+    for(int i = 0; i < numPoints; i++)
+    {
+        float x = arr[3 * i];
+        float y = arr[3 * i + 1];
+        float z = arr[3 * i + 2];
+
+        Eigen::Vector4d v(x,y,z,1);
+        Eigen::Vector4d tv = transformation * v;
+
+        arr[3 * i]     = tv[0];
+        arr[3 * i + 1] = tv[1];
+        arr[3 * i + 2] = tv[2];
+    }
+}
+
 } // namespace lvr
--- src/tools/kaboom/Main.cpp
+++ src/tools/kaboom/Main.cpp
@@ -46,6 +46,7 @@ using namespace std;
 #include <lvr/io/Timestamp.hpp>
 #include <lvr/io/ModelFactory.hpp>
 #include <lvr/io/AsciiIO.hpp>
+#include <lvr/io/IOUtils.hpp>
 #ifdef LVR_USE_PCL
 #include <lvr/reconstruction/PCLFiltering.hpp>
 #endif
@@ -86,75 +87,7 @@ ModelPtr filterModel(ModelPtr p, int k, float sigma)
     return NULL;
 }
 
-size_t countPointsInFile(boost::filesystem::path& inFile)
-{
-    ifstream in(inFile.c_str());
-    cout << timestamp << "Counting points in " << inFile.filename().string() << "..." << endl;
 
-    // Count lines in file
-    size_t n_points = 0;
-    char line[2048];
-    while(in.good())
-    {
-        in.getline(line, 1024);
-        n_points++;
-    }
-    in.close();
-
-    cout << timestamp << "File " << inFile.filename().string() << " contains " << n_points << " points." << endl;
-
-    return n_points;
-}
-
-void writeFrames(Eigen::Matrix4d transform, const boost::filesystem::path& framesOut)
-{
-    std::ofstream out(framesOut.c_str());
-
-    // write the rotation matrix
-    out << transform.col(0)(0) << " " << transform.col(0)(1) << " " << transform.col(0)(2) << " " << 0 << " "
-        << transform.col(1)(0) << " " << transform.col(1)(1) << " " << transform.col(1)(2) << " " << 0 << " "
-        << transform.col(2)(0) << " " << transform.col(2)(1) << " " << transform.col(2)(2) << " " << 0 << " ";
-
-    // write the translation vector
-    out << transform.col(3)(0) << " "
-        << transform.col(3)(1) << " "
-        << transform.col(3)(2) << " "
-        << transform.col(3)(3);
-
-    out.close();
-}
-
-size_t writeModel( ModelPtr model,const  boost::filesystem::path& outfile)
-{
-    size_t n_ip;
-    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
-
-    ModelFactory::saveModel(model, outfile.string());
-
-    return n_ip;
-}
-
-size_t writeAscii(ModelPtr model, std::ofstream& out)
-{
-    size_t n_ip, n_colors;
-
-    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
-
-    ucharArr colors = model->m_pointCloud->getPointColorArray(n_colors);
-    for(int a = 0; a < n_ip; a++)
-    {
-        out << arr[a * 3] << " " << arr[a * 3 + 1] << " " << arr[a * 3 + 2];
-
-        if(n_colors && !(options->noColor()))
-        {
-            out << " " << (int)colors[a * 3] << " " << (int)colors[a * 3 + 1] << " " << (int)colors[a * 3 + 2];
-        }
-        out << endl;
-
-    }
-
-    return n_ip;
-}
 
 size_t writePly(ModelPtr model, std::fstream& out) 
 {
@@ -210,160 +143,6 @@ size_t writePlyHeader(std::ofstream& out, size_t n_points, bool colors)
     out << "end_header" << std::endl;
 }
 
-size_t getReductionFactor(ModelPtr model)
-{
-    size_t n_points;
-
-    int reduction = options->getTargetSize();
-
-    floatArr arr = model->m_pointCloud->getPointArray(n_points);
-    
-
-    std::cout << timestamp << "Point cloud contains " << n_points << " points." << std::endl;
-
-/*
-     * If reduction is less than the number of points it will segfault
-     * because the modulo operation is not defined for n mod 0
-     * and we have to keep all points anyways.
-     * Same if no targetSize was given.
-     */
-    if(reduction != 0)
-    {
-        if(reduction < n_points)
-        {
-            return (int)n_points / reduction;
-        }
-    }
-
-    /* No reduction write all points */
-    return 1;
-
-
-
-}
-
-int asciiReductionFactor(boost::filesystem::path& inFile)
-{
-
-    int reduction = options->getTargetSize();
-
-    /*
-     * If reduction is less than the number of points it will segfault
-     * because the modulo operation is not defined for n mod 0
-     * and we have to keep all points anyways.
-     * Same if no targetSize was given.
-     */
-    if(reduction != 0)
-    {
-        // Count lines in file
-        size_t n_points = countPointsInFile(inFile);
-
-        if(reduction < n_points)
-        {
-            return (int)n_points / reduction;
-        }
-    }
-
-    /* No reduction write all points */
-    return 1;
-
-}
-
-Eigen::Matrix4d buildTransformation(double* alignxf)
-{
-    Eigen::Matrix3d rotation;
-    Eigen::Vector4d translation;
-
-    rotation  << alignxf[0],  alignxf[4],  alignxf[8],
-    alignxf[1],  alignxf[5],  alignxf[9],
-    alignxf[2],  alignxf[6],  alignxf[10];
-
-    translation << alignxf[12], alignxf[13], alignxf[14], 1.0;
-
-    Eigen::Matrix4d transformation;
-    transformation.setIdentity();
-    transformation.block<3,3>(0,0) = rotation;
-    transformation.rightCols<1>() = translation;
-
-    return transformation;
-}
-
-Eigen::Matrix4d getTransformationFromPose(boost::filesystem::path& pose)
-{
-    ifstream poseIn(pose.c_str());
-    if(poseIn.good())
-    {
-        double rPosTheta[3];
-        double rPos[3];
-        double alignxf[16];
-
-        poseIn >> rPos[0] >> rPos[1] >> rPos[2];
-        poseIn >> rPosTheta[0] >> rPosTheta[1] >> rPosTheta[2];
-
-        rPosTheta[0] *= 0.0174533;
-        rPosTheta[1] *= 0.0174533;
-        rPosTheta[2] *= 0.0174533;
-
-        double sx = sin(rPosTheta[0]);
-        double cx = cos(rPosTheta[0]);
-        double sy = sin(rPosTheta[1]);
-        double cy = cos(rPosTheta[1]);
-        double sz = sin(rPosTheta[2]);
-        double cz = cos(rPosTheta[2]);
-
-        alignxf[0]  = cy*cz;
-        alignxf[1]  = sx*sy*cz + cx*sz;
-        alignxf[2]  = -cx*sy*cz + sx*sz;
-        alignxf[3]  = 0.0;
-        alignxf[4]  = -cy*sz;
-        alignxf[5]  = -sx*sy*sz + cx*cz;
-        alignxf[6]  = cx*sy*sz + sx*cz;
-        alignxf[7]  = 0.0;
-        alignxf[8]  = sy;
-        alignxf[9]  = -sx*cy;
-        alignxf[10] = cx*cy;
-
-        alignxf[11] = 0.0;
-
-        alignxf[12] = rPos[0];
-        alignxf[13] = rPos[1];
-        alignxf[14] = rPos[2];
-        alignxf[15] = 1;
-
-        return buildTransformation(alignxf);
-    }
-    else
-    {
-        return Eigen::Matrix4d::Identity();
-    }
-}
-
-Eigen::Matrix4d getTransformationFromFrames(boost::filesystem::path& frames)
-{
-    double alignxf[16];
-    int color;
-
-    std::ifstream in(frames.c_str());
-    int c = 0;
-    while(in.good())
-    {
-        c++;
-        for(int i = 0; i < 16; i++)
-        {
-            in >> alignxf[i];
-        }
-
-        in >> color;
-
-        if(!in.good())
-        {
-            c = 0;
-            break;
-        }
-    }
-
-    return buildTransformation(alignxf);
-}
 
 Eigen::Matrix4d transformFrames(Eigen::Matrix4d frames)
 {
@@ -415,102 +194,8 @@ Eigen::Matrix4d transformFrames(Eigen::Matrix4d frames)
     return frames;
 }
 
-void transformFromOptions(ModelPtr model, int modulo)
-{
-    size_t n_ip, n_colors;
-    size_t cntr = 0;
 
-    floatArr arr = model->m_pointCloud->getPointArray(n_ip);
-    ucharArr colors = model->m_pointCloud->getPointColorArray(n_colors);
 
-    // Plus one because it might differ because of the 0-index
-    // better waste memory for one float than having not enough space.
-    // TO-DO think about exact calculation.
-    size_t targetSize = (3 * ((n_ip)/modulo)) + modulo;
-    floatArr points(new float[targetSize ]);
-    ucharArr newColorsArr;
-
-    if(n_colors)
-    {
-        newColorsArr = ucharArr(new unsigned char[targetSize]);
-    }
-
-    for(int i = 0; i < n_ip; i++)
-    {
-        if(i % modulo == 0)
-        {
-            if(options->sx() != 1)
-            {
-                arr[i * 3] 		*= options->sx();
-            }
-
-            if(options->sy() != 1)
-            {
-                arr[i * 3 + 1] 	*= options->sy();
-            }
-
-            if(options->sz() != 1)
-            {
-                arr[i * 3 + 2] 	*= options->sz();
-            }
-
-            if((cntr * 3) < targetSize)
-            {
-                points[cntr * 3]     = arr[i * 3 + options->x()];
-                points[cntr * 3 + 1] = arr[i * 3 + options->y()];
-                points[cntr * 3 + 2] = arr[i * 3 + options->z()];
-            }
-            else
-            {
-                std::cout << "The following is for debugging purpose" << std::endl;
-                std::cout << "Cntr: " << (cntr * 3) << " targetSize: " << targetSize << std::endl;
-                std::cout << "nip : " << n_ip << " modulo " << modulo << std::endl;
-                break;
-            }
-            
-            if(n_colors)
-            {
-                newColorsArr[cntr * 3]     = colors[i * 3];
-                newColorsArr[cntr * 3 + 1] = colors[i * 3 + 1];
-                newColorsArr[cntr * 3 + 2] = colors[i * 3 + 2];
-            }
-
-            cntr++;
-        }
-    }
-
-    // Pass counter because it is the actual number of points used after reduction
-    // it might be 1 less than the size
-    model->m_pointCloud->setPointArray(points, cntr);
-
-    if(n_colors)
-    {
-        model->m_pointCloud->setPointColorArray(newColorsArr, cntr);
-    }
-}
-
-// transforming with Matrix from frames/pose
-void transformModel(ModelPtr model, Eigen::Matrix4d transformation)
-{
-    cout << timestamp << "Transforming model." << endl;
-    size_t numPoints;
-
-    floatArr arr = model->m_pointCloud->getPointArray(numPoints);
-
-    for(int i = 0; i < numPoints; i++)
-    {
-        float x = arr[3 * i];
-        float y = arr[3 * i + 1];
-        float z = arr[3 * i + 2];
-
-        Eigen::Vector4d v(x,y,z,1);
-        Eigen::Vector4d tv = transformation * v;
-
-        arr[3 * i]     = tv[0];
-        arr[3 * i + 1] = tv[1];
-        arr[3 * i + 2] = tv[2];
-    }
-}
 
 void processSingleFile(boost::filesystem::path& inFile)
 {
@@ -537,30 +222,36 @@ void processSingleFile(boost::filesystem::path& inFile)
         boost::filesystem::path framesPath(frames);
         boost::filesystem::path posePath(pose);
 
-        size_t reductionFactor = getReductionFactor(model);
+        size_t reductionFactor = getReductionFactor(model, options->getTargetSize());
 
         if(options->transformBefore())
         {
-            transformFromOptions(model, reductionFactor);
+ 	    transformAndReducePointCloud(
+		model, reductionFactor,
+		options->sx(), options->sy(), options->sz(),
+		options->x(), options->y(), options->z());	    
         }
 
         if(boost::filesystem::exists(framesPath))
         {
             std::cout << timestamp << "Getting transformation from frame: " << framesPath << std::endl;
             Eigen::Matrix4d transform = getTransformationFromFrames(framesPath);
-            transformModel(model, transform);
+            transformPointCloud(model, transform);
         }
         else if(boost::filesystem::exists(posePath))
         {
 
             std::cout << timestamp << "Getting transformation from pose: " << posePath << std::endl;
             Eigen::Matrix4d transform = getTransformationFromPose(posePath);
-            transformModel(model, transform);
+            transformPointCloud(model, transform);
         }
 
         if(!options->transformBefore())
         {
-            transformFromOptions(model, reductionFactor);
+             transformAndReducePointCloud(
+		model, reductionFactor,
+		options->sx(), options->sy(), options->sz(),
+		options->x(), options->y(), options->z());
         }
 
         static size_t points_written = 0;
@@ -581,7 +272,7 @@ void processSingleFile(boost::filesystem::path& inFile)
                 out.open(options->getOutputFile().c_str(), std::ofstream::out | std::ofstream::trunc);
             }
 
-            points_written += writeAscii(model, out);
+            points_written += writePointsToASCII(model, out, options->noColor());
 
             out.close();
         }
@@ -706,8 +397,13 @@ void processSingleFile(boost::filesystem::path& inFile)
             }
 
             ofstream out(name);
-            transformFromOptions(model, getReductionFactor(model));
-            size_t points_written = writeAscii(model, out);
+           
+	    transformAndReducePointCloud(
+		model, getReductionFactor(model, options->getTargetSize()),
+		options->sx(), options->sy(), options->sz(),
+		options->x(), options->y(), options->z());
+	    
+            size_t points_written = writePointsToASCII(model, out, options->noColor());
 
             out.close();
             cout << timestamp << "Wrote " << points_written << " points to file " << name << endl;
@@ -746,8 +442,13 @@ void processSingleFile(boost::filesystem::path& inFile)
             }
 
             ofstream out(name);
-            transformFromOptions(model, getReductionFactor(model));
-            size_t points_written = writeAscii(model, out);
+	    
+      	    transformAndReducePointCloud(
+		model, getReductionFactor(model, options->getTargetSize()),
+		options->sx(), options->sy(), options->sz(),
+		options->x(), options->y(), options->z());
+
+            size_t points_written = writePointsToASCII(model, out, options->noColor());
 
             out.close();
             cout << timestamp << "Wrote " << points_written << " points to file " << name << endl;
@@ -781,9 +482,12 @@ void processSingleFile(boost::filesystem::path& inFile)
                 writeFrames(transformed, framesOut);
             }
 
+	    transformAndReducePointCloud(
+		model, getReductionFactor(model, options->getTargetSize()),
+		options->sx(), options->sy(), options->sz(),
+		options->x(), options->y(), options->z());
 
-            //transformFromOptions(model, asciiReductionFactor(inFile));
-            transformFromOptions(model, getReductionFactor(model));
+	    
             std::ofstream out;
 
             // write the header -> open in text_mode 
